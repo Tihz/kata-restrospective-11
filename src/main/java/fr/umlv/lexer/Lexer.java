3157048d00c76c33fe4bbe633aa4ee2b1abfce6a
Anthony Guillé <anthony.guille@one2team.com>
2019-04-15 14:08:21 +0200

    Create Lexer

--- src/main/java/fr/umlv/lexer/Lexer.java
+++ src/main/java/fr/umlv/lexer/Lexer.java
@@ -1,3 +1,19 @@
 package fr.umlv.lexer;
 
-// TODO
+import java.util.Objects;
+import java.util.Optional;
+
+class Lexer<T> {
+
+    private static final Lexer LEXER = new Lexer();
+
+    static Lexer create() {
+        return LEXER;
+    }
+
+    Optional<T> tryParse(String value) {
+        Objects.requireNonNull(value);
+        return Optional.empty();
+    }
+
+}
